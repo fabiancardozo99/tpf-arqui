0d68916f72f146fac5ba6dfafb8837aa9da8c6fd
Fabian <fabiancardozo99@gmail.com>
2022-11-23 01:44:23 -0300

    tpf terminado

--- tpf.ino
+++ tpf.ino
@@ -23,7 +23,7 @@
 
 const int B = 4275; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
-const int pinTempSensor = 5; // Grove - Temperature Sensor connect to A0
+const int pinTempSensor = 34; // Grove - Temperature Sensor connect to A0
  
 #if defined(ARDUINO_ARCH_AVR)
 #define debug Serial
@@ -70,24 +70,24 @@ void loop() {
 
   int a = analogRead(pinTempSensor);
  
-  float R = 1023.0/a-1.0;
+  float R = 4095.0/a-1.0;
   R = R0*R;
 
   float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
 
-  Serial.print("temperature = ");
-  Serial.println(temperature);
-  Serial.println(a);
-
-  delay(100);
-
-  // Serial.print("sending -> ");
+  // Serial.print("temperature = ");
   // Serial.println(temperature);
-  // analog->save(temperature);
+  // Serial.println(a);
+
+  // delay(1000);
+
+  Serial.print("sending -> ");
+  Serial.println(temperature);
+  analog->save(temperature);
 
   // wait three seconds (1000 milliseconds == 1 second)
   //
   // because there are no active subscriptions, we can use delay()
   // instead of tracking millis()
-  // delay(3000);
+  delay(6000);
 }
